748484e777ea0659809b496ed493bf25c04ecaa3
Luka Ernestini <luka.ernestini@gmail.com>
2017-12-03 20:27:13 +0100

    Added value for amount of free space in class Prevoz, implemented getters/setters, updated declarations in activites.
    popup of Prevoz now has the correct values for the element of the list selected!
    
    #TODO make phone number clickable

--- app/src/main/java/nameplaceholder/prevazanjaorg/ListPrevozovActivity.java
+++ app/src/main/java/nameplaceholder/prevazanjaorg/ListPrevozovActivity.java
@@ -1,11 +1,14 @@
 package nameplaceholder.prevazanjaorg;
 
+import android.annotation.SuppressLint;
 import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.util.Linkify;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
@@ -23,6 +26,7 @@ import android.support.v7.widget.Toolbar;
 import android.widget.PopupWindow;
 import android.widget.SearchView;
 import android.widget.SearchView.OnQueryTextListener;
+import android.widget.TextView;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
@@ -50,20 +54,17 @@ public class ListPrevozovActivity extends AppCompatActivity implements OnQueryTe
         setSupportActionBar(myToolbar);
 
         //POPULATE AKTIVNI PREVOZI
-        String dateTime = "29.11.2017 16:00:00";
+        final String dateTime = "29.11.2017 16:00:00";
         DateTime trenutniCas = new DateTime(); //trenutni datum in točen čas
         DateTimeFormatter dtf = DateTimeFormat.forPattern("dd.MM.yyyy HH:mm:ss");
         DateTime drugCas = dtf.parseDateTime(dateTime);
-        Prevoz dummyPrevoz = new Prevoz("Maribor", "Koper", "040202108", 10.0, 4, false, "Toyota Yaris črne barve", "Luka", trenutniCas);
-        Prevoz dummyPrevoz2 = new Prevoz("Ljubljana", "Maribor", "040256339", 5.0, 3, false, "Toyota Hilux", "Žiga", drugCas);
-        Prevoz dummyPrevoz3 = new Prevoz("Celje", "Novo Mesto", "04025897464", 7.0, 4, true, "Mazda 3", "Anja", drugCas.plusDays(2));
+        Prevoz dummyPrevoz = new Prevoz("Maribor", "Koper", "040202108", 10.0, 3, 4, false, "Toyota Yaris črne barve", "Luka", trenutniCas);
+        Prevoz dummyPrevoz2 = new Prevoz("Ljubljana", "Maribor", "040256339", 5.0, 3, 3, false, "Toyota Hilux", "Žiga", drugCas);
+        Prevoz dummyPrevoz3 = new Prevoz("Celje", "Novo Mesto", "04025897464", 7.0, 1, 4, true, "Mazda 3", "Anja", drugCas.plusDays(2));
         aktivniPrevozi.add(dummyPrevoz2);
         aktivniPrevozi.add(dummyPrevoz3);
         //for (Integer i=0; i<20;i++)
             aktivniPrevozi.add(dummyPrevoz);
-
-
-
 
 
         //TESTIRANJE
@@ -71,9 +72,12 @@ public class ListPrevozovActivity extends AppCompatActivity implements OnQueryTe
         final ListView listViewPrevozov = (ListView) findViewById(R.id.seznamPrevozov);
         listViewPrevozov.setAdapter(listAdapterPrevozov);
 
+
         frameLayout = (FrameLayout) findViewById(R.id.list_relative);
         frameLayout.getForeground().setAlpha(0); // Črn foreground nardim transparenten
+
         listViewPrevozov.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
@@ -91,9 +95,26 @@ public class ListPrevozovActivity extends AppCompatActivity implements OnQueryTe
                 popupWindow.setBackgroundDrawable(new BitmapDrawable());// Ker na Android 5.0.1 ne deluje dismiss popupa, poskusim s tem pristopom
                 popupWindow.setOutsideTouchable(true);// <-----
 
-                /*TextView ime = (TextView) frameLayout.findViewById(R.id.pop_ime);
+                //   Nastavljanje vrednosti v popupu
                 Prevoz p = (Prevoz) adapterView.getItemAtPosition(i);
-                ime.setText(p.getIme());*/
+                // Pridobivanje View-ov
+                TextView ime = (TextView) container.findViewById(R.id.pop_ime);
+                TextView stevilka = (TextView) container.findViewById(R.id.pop_telefon);
+                TextView datum = (TextView) container.findViewById(R.id.pop_datum);
+                TextView ura = (TextView) container.findViewById(R.id.pop_ura);
+                TextView prostaMesta = (TextView) container.findViewById(R.id.pop_prostaMesta);
+                TextView opis = (TextView) container.findViewById(R.id.pop_opis);
+
+                // Prireditev vrednosti
+                ime.setText(p.getIme());
+                stevilka.setText(p.getMobitel());
+                datum.setText(p.getDatum());
+                ura.setText(p.getCas());
+                prostaMesta.setText(p.getOseb() + "/" + p.getMaxOseb());
+                opis.setText(p.getOpis());
+
+                if (p.getOseb() == 1 || p.getOseb() == 0)
+                    prostaMesta.setTextColor(Color.RED);
 
                 popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
                     @Override
